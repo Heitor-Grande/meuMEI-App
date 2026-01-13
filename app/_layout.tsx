c9c8d08975bf96d696aa8db2708f7b53a37c6018
Heitor-Grande <heitorogrande2001@gmail.com>
2026-01-12 21:03:42 -0300

    authLyout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout() {
           isLogado ?
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             :
-            <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         }
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
